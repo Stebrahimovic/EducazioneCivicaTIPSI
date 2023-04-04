fd60513cf4ff55a04ef3ceb156609f1d16d233ce
Stebrahimovic <126584787+Stebrahimovic@users.noreply.github.com>
2023-04-04 17:00:16 +0200

    Punto 2 semaforo rosso

--- Python.py
+++ Python.py
@@ -11,9 +11,18 @@ def Punto1(s,n):
     except:
         return False
 
+def Punto2(s,n):
+
+    return False
+
+
 s = input('Inserisci s: ')
 n = input('Inserisci n: ')
 
 if not Punto1(s,n):
     print('Punto 1 rosso')
+    exit()
+
+if not Punto2(s,n):
+    print('Punto 2 rosso')
     exit()
