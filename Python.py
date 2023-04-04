b77c7a4729246244e01216802dfb202360ee76a4
Stebrahimovic <126584787+Stebrahimovic@users.noreply.github.com>
2023-04-04 16:54:55 +0200

    Punto 1 semaforo verde

--- Python.py
+++ Python.py
@@ -1,7 +1,19 @@
-def Punto1():
+def Punto1(s,n):
+    
+    word = ""
+    try:
+        n=int(n)
+        for l in s:
+            word += chr(ord(l)+n)
 
-    return False
+        print('Punto 1:',s,word)
+        return True
+    except:
+        return False
 
-if not Punto1():
+s = input('Inserisci s: ')
+n = input('Inserisci n: ')
+
+if not Punto1(s,n):
     print('Punto 1 rosso')
     exit()
