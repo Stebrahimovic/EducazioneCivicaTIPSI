c00c1fbc3cff51dc06427ab3ee5faf35420a311e
Stebrahimovic <126584787+Stebrahimovic@users.noreply.github.com>
2023-04-04 17:07:04 +0200

    Punto 2 semaforo verde

--- Python.py
+++ Python.py
@@ -13,16 +13,32 @@ def Punto1(s,n):
 
 def Punto2(s,n):
 
-    return False
+    word = ""
+    try:
+        n=int(n)
+        for l in s:
+            word += chr(ord(l)-n)
 
+        print('Punto 1:',s,word)
+        return True
+    except:
+        return False
 
-s = input('Inserisci s: ')
-n = input('Inserisci n: ')
+try:
+    punto=int(input('Quale punto vuoi eseguire: '))
 
-if not Punto1(s,n):
-    print('Punto 1 rosso')
-    exit()
-
-if not Punto2(s,n):
-    print('Punto 2 rosso')
-    exit()
+    match punto:
+        case 1:
+            s = input('Inserisci s: ')
+            n = input('Inserisci n: ')
+            if not Punto1(s,n):
+                print('Punto 1 rosso')
+                exit()
+        case 2:
+            if not Punto2(s,n):
+                s = input('Inserisci s: ')
+                n = input('Inserisci n: ')
+                print('Punto 2 rosso')
+                exit()
+except:
+    print('Inserisci un numero consentito...')
